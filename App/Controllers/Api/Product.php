1235b3a5206a93cd84db8ab56d43b765ab8c345f
kapa16 <kapa16@inbox.ru>
2019-03-23 00:24:24 +0500

    api controller

--- App/Controllers/Api/Product.php
+++ App/Controllers/Api/Product.php
@@ -2,7 +2,15 @@
 
 namespace App\Controllers\Api;
 
-class Product
-{
+use App\Controllers\Controller;
 
+class Product extends Controller
+{
+    public function __invoke()
+    {
+        $limitFrom = +$_GET['from'] ?? 0;
+        $limitTo = +$_GET['to'] ?? 0;
+        $products = \App\Models\Products\Product::getLimit($limitFrom, $limitTo);
+        echo json_encode($products);
+    }
 }
